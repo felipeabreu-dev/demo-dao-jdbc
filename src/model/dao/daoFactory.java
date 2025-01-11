3330fbbe2f7091a574053f2c101b0c3bd29e55c7
Felipe Abreu <abreufelipe12@hotmail.com>
2025-01-10 21:49:29 -0300

    findById implementation and update db.properties

--- src/model/dao/daoFactory.java
+++ src/model/dao/daoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.implement.SellerDaoJDBC;
 
 public class daoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
