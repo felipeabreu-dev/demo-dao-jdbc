542c52591e8403e280a92f133d7dd5f9461953c6
Felipe Abreu <abreufelipe12@hotmail.com>
2025-01-11 13:25:43 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,6 +33,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! Seller id: " + newSeller.getId());
+		
+		System.out.println("\n==== TESTE 5: Seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
